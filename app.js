65d9671592c5b91c2a2a241e0b8a6e69dab2aab6
Akash Sharma <shara98@mcmaster.ca>
2021-09-02 15:56:56 -0400

    modified app.js

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ form.addEventListener('submit', async function (e) {
     const searchTerm = form.elements.query.value;
     const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
 })
+
+const makeImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement("IMG");
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
