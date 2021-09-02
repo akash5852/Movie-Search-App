b033d44908e2c499cb12fafa0546b355fb7b2610
Akash Sharma <shara98@mcmaster.ca>
2021-09-02 19:01:50 -0400

    quick fix

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function (e) {
+    var movieArea = document.getElementById("movieArea");
+    movieArea.innerHTML = " "
     e.preventDefault();
     const searchTerm = form.elements.query.value;
     const config = { params: { q: searchTerm } }
     const res = await axios.get(`https://api.tvmaze.com/search/shows`, config)
-    makeImages(res.data);
+    makeImages(res.data)
+    res.data = {};
     form.elements.query.value = "";
 })
 
 const makeImages = (shows) => {
+    console.log(shows)
     for (let result of shows) {
         if (result.show.image) {
             const img = document.createElement("IMG");
             img.src = result.show.image.medium;
-            document.body.append(img);
+            var movieArea = document.getElementById("movieArea");
+            movieArea.append(img)
         }
     }
 }
